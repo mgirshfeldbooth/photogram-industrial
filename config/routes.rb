2062a12f3b14eda0388e6b6a1f69602693f17260
mgirshfeldbooth <mgirshfeld@chicagobooth.edu>
2023-01-15 23:15:47 +0000

    Starting feed view

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,12 @@ Rails.application.routes.draw do
   resources :comments
   resources :photos
   
+  get ":username/liked" => "photos#liked", as: :liked_photos
+  get ":username/feed" => "photos#feed", as: :feed_photos
+  get ":username/followers" => "users#followers", as: :followers
+  get ":username/following" => "users#leaders", as: :leaders
+  
 
-
-  get "/:username" => "users#show"
+  get "/:username" => "users#show", as: :user
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
