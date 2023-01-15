1b7b4bfcc27996e3320c52e4bc2108e6656483fd
mgirshfeldbooth <mgirshfeld@chicagobooth.edu>
2023-01-15 16:55:15 +0000

    Added indirect associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,10 +28,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :own_photos, class_name: "Photo", foreign_key: "owner_id"
-  has_many :comments, foreign_key: "author_id"
-  has_many :sent_follow_requests, class_name: "FollowRequest", foreign_key: "sender_id"
-  has_many :recieved_follow_requests, class_name: "FollowRequest", foreign_key: "recipient_id"
+  has_many :own_photos, class_name: "Photo", foreign_key: :owner_id
+  has_many :comments, foreign_key: :author_id
+
+  has_many :sent_follow_requests, class_name: "FollowRequest", foreign_key: :sender_id
+  has_many :accepted_sent_follow_requests, -> { where(status: "accepted" ) }, class_name: "FollowRequest", foreign_key: :sender_id
+
+  has_many :recieved_follow_requests, class_name: "FollowRequest", foreign_key: :recipient_id
+  has_many :accepted_recieved_follow_requests, -> { where(status: "accepted" ) }, class_name: "FollowRequest", foreign_key: :recipient_id
+
   has_many :likes, foreign_key: :fan_id
+  has_many :liked_photos, through: :likes, source: :photo
+
+  has_many :leaders, through: :accepted_sent_follow_requests, source: :recipient
+  has_many :followers, through: :accepted_recieved_follow_requests, source: :sender
+  has_many :feed, through: :leaders, source: :own_photos
+  has_many :discover, through: :leaders, source: :liked_photos
+
   
 end
